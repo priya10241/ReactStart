c4824b870ddb6ad597efac8a3e9ceaa6fd6447c3
Priya <priyay1104@gmail.com>
2024-12-16 22:34:55 +0530

    "change"

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useCallback,useEffect, useRef} from 'react'
 import './App.css'
-
 function App() {
   const [length,setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
