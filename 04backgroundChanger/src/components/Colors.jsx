7da113f15e1457762f7e19c13b5fd8788a6db975
Priya <priyay1104@gmail.com>
2024-12-16 22:26:00 +0530

    "changes"

--- 04backgroundChanger/src/components/Colors.jsx
+++ 04backgroundChanger/src/components/Colors.jsx
@@ -6,17 +6,17 @@ function Colors(){
         <>
        <div className="w-full h-screen" style = {{ backgroundColor: color}}>
         <div className="flex inline bg-white pl-40 pr-10 rounded">
-        <button className="bg-orange-800 p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("orange")}>Orange</button>
-        <button className="bg-red-800 p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("red")}>Red</button>
-        <button className="bg-blue-800 p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("blue")}>Blue</button>
-        <button className="bg-green-800 p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("green")}>Green</button>
-        <button className="bg-pink-800 p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("pink")}>Pink</button>
-        <button className="bg-white p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("white")}>White</button>
-        <button className="bg-black text-white p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("black")}>Black</button>
-        <button className="bg-yellow-400 p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("yellow")}>Yellow</button>
-        <button className="bg-gray-400 p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("gray")}>Gray</button>
-        <button className="bg-indigo-800 p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("indigo")}>Indigo</button>
-        <button className="bg-purple-800 p-2 m-3 border-2 border-gradient rounded-2xl" onClick={()=>setColor("purple")}>Purple</button>     
+        <button className="p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"orange"}} onClick={()=>setColor("orange")}>Orange</button>
+        <button className="p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"red"}} onClick={()=>setColor("red")}>Red</button>
+        <button className=" p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"blue"}} onClick={()=>setColor("blue")}>Blue</button>
+        <button className=" p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"green"}} onClick={()=>setColor("green")}>Green</button>
+        <button className="p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"pink"}} onClick={()=>setColor("pink")}>Pink</button>
+        <button className=" p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"white"}} onClick={()=>setColor("white")}>White</button>
+        <button className="text-white p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"black"}} onClick={()=>setColor("black")}>Black</button>
+        <button className=" p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"yellow"}} onClick={()=>setColor("yellow")}>Yellow</button>
+        <button className=" p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"gray"}} onClick={()=>setColor("gray")}>Gray</button>
+        <button className=" p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"indigo"}} onClick={()=>setColor("indigo")}>Indigo</button>
+        <button className=" p-2 m-3 border-2 border-gradient rounded-2xl" style = {{background:"purple"}} onClick={()=>setColor("purple")}>Purple</button>     
         </div>
         </div>
         </> 
