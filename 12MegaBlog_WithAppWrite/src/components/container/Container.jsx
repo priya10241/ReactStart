a856b55d78ec304207095bd5ec53426c1e9880b7
Priya <priyay1104@gmail.com>
2025-01-26 23:48:01 +0530

    "Added all components"

--- 12MegaBlog_WithAppWrite/src/components/container/Container.jsx
+++ 12MegaBlog_WithAppWrite/src/components/container/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Container({Children}){
     return(
-        <div className="w-80% bg-gray-800 text-white">
+        <div className="w-80% max-w-7xl mx-auto bg-gray-800 text-white">
             {Children}
         </div>
     )
